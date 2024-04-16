2f318b19a512d8178958a8eaece6a773a8f8f379
Cho2982 <162522355+Cho2982@users.noreply.github.com>
2024-04-16 20:41:24 +0900

    Week6.py

---
+++ Week6.py
@@ -0,0 +1,18 @@
+##1번
+출력 함수: print(), write(), writeline()
+입력 함수: input(), read(), readline(), readlines()
+
+##2번
+답: A - C - B - D
+
+##3번
+답: 3 (a는 쓰기모드이다. 기존 파일 있으면 삭제하고 새로 만든다.)
+
+##4번
+답: 1 - open
+    2 - readline
+    3 - close
+
+##6번
+답: 1 - inFp.readlines()
+    2 - outFp.write(inStr)
